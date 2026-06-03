140c981e97f1df34363b66c594442591fb2d1324
satasun5 <satasun5@gmail.com>
2026-06-03 23:17:45 +0900

    Load responsive preview css

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./responsive-preview.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = document.getElementById("root");
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
